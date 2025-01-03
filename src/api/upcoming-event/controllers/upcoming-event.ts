78c090d6cf0891163a647c2c4b2b107b48078a19
SabbirHasan07 <sabbirhasan.engr@gmail.com>
2025-01-04 00:53:20 +0600

    update

--- src/api/upcoming-event/controllers/upcoming-event.ts
+++ src/api/upcoming-event/controllers/upcoming-event.ts
@@ -2,6 +2,18 @@
  * upcoming-event controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::upcoming-event.upcoming-event');
+export default factories.createCoreController('api::upcoming-event.upcoming-event', ({ strapi }) => ({
+  async find(ctx) {
+    // Adding populate=* to populate all relations
+    ctx.query = {
+      ...ctx.query,
+      populate: '*',
+    };
+
+    // Call the default core controller's find method
+    const { data, meta } = await super.find(ctx);
+    return { data, meta };
+  },
+}));
